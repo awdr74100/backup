0c2923b1ed58adefae8937b63a2a8881525cbd35
藍奕濡 <a78945612385238@gmail.com>
2019-02-27 17:14:27 +0800

    js test

---
+++ JavaScript/ch01/js/c01.js
@@ -0,0 +1,21 @@
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+
+if (hourNow > 18) 
+{
+    greeting = "晚安";    
+}else if (hourNow > 12) 
+{
+    greeting = "午安";
+}else if (hourNow > 0)
+{
+    greeting = '早安';
+}else
+{
+    greeting = "歡迎";
+}
+
+document.write('<h3>' + greeting + '</h3>');
+
+//直接顯示頁面
---
+++ JavaScript/ch03/js/c03.js
@@ -0,0 +1,26 @@
+function back (width,height) {
+    var total = width*height;
+    return total;
+}
+
+var c = back(6,9);
+
+
+console.log(c);
+
+function git (){
+    console.log("測試正常");
+}
+
+git();
+
+var room = {
+    number:40,
+    people:12,
+    
+}
+
+var number = room.number;
+
+console.log(number);
+
