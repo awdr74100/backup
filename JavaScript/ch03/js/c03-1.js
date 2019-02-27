2c63aefdc93474995e19e96f2cf0df9f40e9144c
藍奕濡 <a78945612385238@gmail.com>
2019-02-27 23:31:06 +0800

    20190227

---
+++ JavaScript/ch03/js/c03-1.js
@@ -0,0 +1,14 @@
+var aa  = [
+    3,4,5
+
+];
+
+console.log(aa[0]);
+
+cost = {
+    t1:40,
+    t2:50,
+    t3:60
+}
+
+cost.t1.items[0];
--- JavaScript/ch03/js/c03.js
+++ JavaScript/ch03/js/c03.js
@@ -17,10 +17,38 @@ git();
 var room = {
     number:40,
     people:12,
+    git_totle:function(){
+        return (this.number - this.people);
+    }
     
 }
 
 var number = room.number;
-
+//delete room.number;
 console.log(number);
+console.log(room.git_totle());
+
+
+var a = new Object();
+a.name = "blue";
+a.number = 45;
+a.total = function(){
+    return this.name + this.number;
+};
+
+console.log(a.total());
+
+function test3 (number,name){
+    this.number = number;
+    this.name = name;
+    this.total = function tt() {
+        return (number+name);
+    }
+}
+
+var t1 = new test3(14,"帥哥");
+var t2 = t1.name;
+    t2 += t1.total();
+
+console.log(t2);
 
