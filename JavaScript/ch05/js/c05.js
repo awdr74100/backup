858f01ffe9196d243313444baa26c246f586cfd6
藍奕濡 <a78945612385238@gmail.com>
2019-03-05 01:14:11 +0800

    20190305

---
+++ JavaScript/ch05/js/c05.js
@@ -0,0 +1,15 @@
+var a = document.getElementById("aaa");
+a.textContent = "更改成功";
+a.className = "cc";
+
+var b = document.querySelector(".bb");
+b.textContent = "更改成功";
+
+var c = document.getElementsByClassName(".cc");
+c.textContent = "更改成功";
+
+var d = document.getElementsByTagName("p");
+d.textContent = "更改成功";
+
+var e = document.querySelectorAll(".ee");
+e.textContent = "更改成功";
