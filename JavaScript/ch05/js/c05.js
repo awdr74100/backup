ec0bdb487a2a6f83aa568dff1e4ec640a9cedb42
藍奕濡 <a78945612385238@gmail.com>
2019-03-07 01:10:35 +0800

    20190307

--- JavaScript/ch05/js/c05.js
+++ JavaScript/ch05/js/c05.js
@@ -4,6 +4,7 @@ a.textContent = "第一行更改成功";
 //a.className = "change";
 
 var b = document.getElementsByClassName("hot");
+
 console.log(b);
 
 var c = document.getElementsByTagName("li");
@@ -29,6 +30,26 @@ var cc = document.querySelectorAll(".hot");
 for(var i =0;i<cc.length;i++){
     console.log(i);
 }
+
+var dd = document.getElementById("one");
+//dd.nextSibling.textContent = "更改成功";
+
+var ee = document.getElementById("zero");
+var test =  ee.firstChild.nextSibling.nodeValue ;
+console.log(test);
+
+var ff = document.getElementById("one").innerHTML;
+console.log(ff);
+
+var add = document.createElement("li");
+var addtext = document.createTextNode("加入成功囉");
+add.appendChild(addtext);
+var addnode = document.getElementsByClassName("hot")[3];
+addnode.appendChild(add);
+
+var remove = document.getElementById("two");
+var dade = remove.parentNode;
+dade.removeChild(remove);
 
 
 
