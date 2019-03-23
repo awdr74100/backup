5925a8ebe8c004b446de6c49e42f68691b3b2ea5
藍奕濡 <a78945612385238@gmail.com>
2019-03-24 01:28:00 +0800

    20190324

--- JavaScript/ch11/js/c11-1.js
+++ JavaScript/ch11/js/c11-1.js
@@ -1,3 +1,9 @@
 $(document).ready(function () {
-    
+    $(".on").on("mouseover",".t1", function (e) {
+        e.preventDefault();
+        $(this)
+            .next(".a1")
+            .not(":animated")
+            .slideToggle();
+    });
 });
