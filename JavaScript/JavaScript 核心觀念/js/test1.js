bf0822a50addbf649aaf15d9f419eb823f552c7e
藍奕濡 <a78945612385238@gmail.com>
2019-04-02 01:52:07 +0800

    20190402

---
+++ JavaScript/JavaScript 核心觀念/js/test1.js
@@ -0,0 +1,13 @@
+var testValue = 'outer';
+
+function foo() {
+  console.log(testValue);		// "outer"
+}
+
+function bar() {
+  var testValue = 'inner';
+  console.log(testValue);
+  foo();
+}
+
+bar();
