31fabff978981cbab9670b30ac0240438f23cbd9
藍奕濡 <a78945612385238@gmail.com>
2019-05-07 20:51:07 +0800

    20190507

--- Responsive Web Design/parallax scroiling/js/main.js
+++ Responsive Web Design/parallax scroiling/js/main.js
@@ -1,32 +1,87 @@
 $(document).ready(function () {
-    $(".scrollTop").click(function (e) {
-        e.preventDefault();
-        var target = $(this).attr("href");
-        var targetPos = $(target).offset().top;
-        $("html,body").animate({
-            scrollTop: targetPos
-        }, 1000)
-        // console.log(target);
+    var showSkill = false;
 
+
+    // 動畫移動到錨點位置 start
+
+    $('.scrollTop').click(function (e) {
+        e.preventDefault();
+        var target = $(this).attr('href');
+        var targetPos = $(target).offset().top;
+        // console.log(targetPos);
+        $('html, body').animate({
+            scrollTop: targetPos
+        }, 1000);
     });
+
+    // 動畫移動到錨點位置 end
+
+
+
+
+    //y軸移動時的觸發 start
 
     $(window).scroll(function () {
         var scrollPos = $(window).scrollTop();
-        console.log(scrollPos);
+        var windowHeight = $(window).height();
+        console.log(windowHeight);
+        // console.log(scrollPos, windowHeight);
 
-        $(".scrollTop").each(function () {
-            var target = $(this).attr("href");
+        $('.scrollTop').each(function () {
+            var target = $(this).attr('href');
             var targetPos = $(target).offset().top;
             var targetHeight = $(target).outerHeight();
-            console.log(target, targetPos, targetHeight);
-
             if (targetPos - 1 <= scrollPos && (targetPos + targetHeight) > scrollPos) {
-                //   console.log(target);
-                $(".scrollTop").removeClass("active");
-                $(this).addClass("active");
+                $('.scrollTop').removeClass('active')
+                $(this).addClass('active');
             } else {
-                $(this).removeClass("active");
+                $(this).removeClass('active')
             }
         });
+
+        //y軸移動時的觸發 end
+
+
+
+
+        //技能條動畫顯示跑動 start
+
+        var skillTop = $('#skills').offset().top;
+        // console.log('skillTop', skillTop);
+        if (skillTop <= (scrollPos + windowHeight / 2) && !showSkill) {
+            showSkill = true;
+            $('#skills .progress-bar').each(function () {
+                var thisValue = $(this).data('progress');
+                // console.log('thisValue', thisValue);
+                $(this).css('width', thisValue + '%');
+            });
+        }
+
+        //技能條動畫顯示跑動 end
+
+
+
+
+        //物件淡入淡出效果 start
+
+        $('.animated').each(function () {
+            var thisPos = $(this).offset().top;
+            if ((windowHeight + scrollPos) >= thisPos) {
+                $(this).addClass('fadeIn');
+            }
+        });
+
+        //物件淡入淡出效果 end
+
+
+
+
+        //指定物件偏移 end
+
+        $('#profiles').css('background-position-y', (scrollPos / 2) + 'px')
+        $('#header-ele').css('transform', 'translateY( ' + (scrollPos / 2) + 'px )')
+
+        //指定物件偏移 end
     });
+
 });
