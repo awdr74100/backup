1b9ab19ad920ccf405723c6073cba0959d1d032b
藍奕濡 <a78945612385238@gmail.com>
2019-05-31 20:58:55 +0800

    20190531

--- Sass/Sass  BEM project/js/main.js
+++ Sass/Sass  BEM project/js/main.js
@@ -3,4 +3,20 @@ $(document).ready(function () {
         e.preventDefault();
         $("body").toggleClass("mobile");
     });
+    $(".js-click-1").on("click", function () {
+        $(this).toggleClass("card__icon--active");
+    });
+    $(".js-click-2").on("click", function () {
+        $(this).toggleClass("card__icon--active");
+    });
+    $(".js-click-3").on("click", function () {
+        $(this).toggleClass("card__icon--active");
+    });
+    $(".js-click-4").on("click", function () {
+        $(this).toggleClass("card__icon--active");
+    });
+    $(".js-link-register").on("click", function (e) {
+        e.preventDefault();
+        window.location.href = "register.html"
+    });
 });
