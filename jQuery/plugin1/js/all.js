65c7aca65ef5d619cb01739be7af2bc1745520f7
a78945612385238@gmail.com <a78945612385238@gmail.com>
2019-07-02 22:51:28 +0800

    20190702

---
+++ jQuery/plugin1/js/all.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    lightbox.option({
+        'resizeDuration': 200,
+        'wrapAround': true,
+        'fadeDuration':600,
+    })
+});
---
+++ jQuery/plugin2/js/all.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    var mySwiper = new Swiper('.swiper-container', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        autoplay:{
+            delay:2000,
+        },
+        effect:"slide",
+        
+
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        // And if we need scrollbar
+        // scrollbar: {
+        //     el: '.swiper-scrollbar',
+        // },
+    })
+});
