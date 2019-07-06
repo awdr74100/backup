0e39c039b15bf779f8158b3da44bc452828eb327
藍奕濡 <a78945612385238@gmail.com>
2019-07-06 23:03:50 +0800

    20190706 file sort out

--- JavaScript/ch03/js/c03-2.js
+++
@@ -1,8 +0,0 @@
-window.print();
-
-var size = document.getElementById('size');
-size.textContent = window.screen.height;
-
-var size = document.getElementById('size');
-size.textContent = document.lastModified;
-
--- JavaScript/ch11/js/c11-1.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $(".on").on("mouseover",".t1", function (e) {
-        e.preventDefault();
-        $(this)
-            .next(".a1")
-            .not(":animated")
-            .slideToggle();
-    });
-});
