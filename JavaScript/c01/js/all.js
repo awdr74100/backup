49d5bcac1b1aa6717124a04575671738437fe331
藍奕濡 <a78945612385238@gmail.com>
2019-07-10 01:19:54 +0800

    20190710 JavaScript start

---
+++ JavaScript/c01/js/all.js
@@ -0,0 +1,14 @@
+var price = 40;
+document.getElementById("price").textContent = price;
+
+var name = "藍奕濡 "
+var ex = "帥哥"
+var total = name + ex;
+document.getElementById("name").textContent = total+" 你好";
+
+var a = 10;
+var b = 20;
+var c = a - b;
+document.getElementById("totle").textContent = c;
+console.log(c);
+
---
+++ JavaScript/c02/js/all.js
@@ -0,0 +1,7 @@
+function greet (){
+    console.log("嗨")
+    console.log("請問你想要點什麼？")
+}
+
+greet();
+
