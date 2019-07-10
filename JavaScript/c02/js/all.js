353277a19e4703f003102b3fd53604d7cdbbca31
藍奕濡 <a78945612385238@gmail.com>
2019-07-10 17:08:01 +0800

    20190710

--- JavaScript/c02/js/all.js
+++ JavaScript/c02/js/all.js
@@ -1,7 +1,17 @@
-function greet (){
-    console.log("嗨")
-    console.log("請問你想要點什麼？")
+var btn = document.getElementById("countId");
+btn.onclick = function () {
+    // 餐點價格
+    var hamPrice = 50;
+    var cokePrice = 20;
+    // 取得餐點數目
+    var hamNum = parseInt(document.getElementById("hamNumId").value);
+    var cokeNum = parseInt(document.getElementById("cokeNumId").value);
+    // 計算餐點價格
+    var hamTotal = hamPrice * hamNum;
+    var cokeTotal = cokePrice * cokeNum;
+    var total = hamTotal + cokeTotal;
+    // 輸出
+    document.querySelector(".total").textContent = total;
 }
 
-greet();
 
