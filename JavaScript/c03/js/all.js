d350dbdf1db55b3eb926e5dcdc390f2dac12d59a
藍奕濡 <a78945612385238@gmail.com>
2019-07-12 23:32:04 +0800

    20190712

--- JavaScript/c03/js/all.js
+++ JavaScript/c03/js/all.js
@@ -1,8 +1,10 @@
-var cornField = [8,5,6];
-console.log(typeof(cornField));
+// ---------------基本陣列宣告
+var cornField = [8, 5, 6];
+console.log(typeof (cornField));
 var cornField3 = cornField[2];
 console.log(cornField3);
-// ----------------
+console.log("--------------------------");
+// ----------------陣列基本用法
 var box = [];
 box.push(5);
 box.push(8); //push最後面新增
@@ -10,7 +12,50 @@ box.push(6);
 console.log(box);
 box[0] = 9;
 box[3] = 10; //另類新增
-box.splice(2,2);
-
+box.splice(2, 2);
 console.log(box);
 console.log(box.length);
+console.log("--------------------------");
+// -----------------物件宣告
+var farm = {
+    farmer: "藍奕濡",
+    dog: ["阿喵", "阿威"],
+    chick: 8,
+    count: 5,
+    warring: function () {
+        console.log(farm.farmer + " 該回家吃飯囉");
+    },
+
+}
+farm.count = 10;
+farm.farmer = "roya";
+farm.box = [5, 6, 1];
+farm.godinner = function () {
+    var a = farm.chick + farm.count;
+    return a;
+};
+console.log(farm.dog[0]); //物件-陣列-取值
+console.log(farm.warring());
+console.log(farm.godinner());
+console.log(farm);
+console.log("--------------------------");
+// -----------------大型物件
+var farms = [{
+    farmer: "卡斯柏",
+    dogs: ["詹姆士", "龐德"],
+    chick: 15,
+    cornField: [8, 5, 6],
+    broccoliField: [6, 6, 6, 6],
+    scarecrow: 9,
+}, {
+    farmer: "查理",
+    dogs: ["皮皮"],
+    chick: 30,
+    cornField: [18, 612],
+    broccoliField: [8, 8, 8],
+    scarecrow: 6,
+}]
+
+console.log(farms);
+console.log(farms[1].dogs[0]);
+
