e93e2370108ad3cba6c5b38d047f3b543f0489c9
藍奕濡 <a78945612385238@gmail.com>
2019-07-19 01:08:51 +0800

    20190719

--- JavaScript/c08/js/all.js
+++ JavaScript/c08/js/all.js
@@ -1,17 +1,22 @@
 var el = document.querySelector(".btn");
 var body = document.querySelector(".body");
+var title = document.querySelector(".title a");
 
 // el.onclick = function(e){
 //     console.log(e);
 // }
 
-el.addEventListener("click",function(e){
+el.addEventListener("click", function (e) {
     alert("el");
     e.stopPropagation();
-},false)
 
-body.addEventListener("click",function(e){
-    alert("body");
-    
-},true);
+}, false)
 
+// body.addEventListener("click",function(e){
+//     alert("body");
+
+// },false);
+
+title.addEventListener("click", function (e) {
+    e.preventDefault();
+})
