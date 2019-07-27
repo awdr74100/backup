a9fe169d6e3b97828cd2f0ee2e7729fe47836924
藍奕濡 <a78945612385238@gmail.com>
2019-07-27 20:15:18 +0800

    20190727 ES6 update

---
+++ JavaScript/c18/js/all.js
@@ -0,0 +1,26 @@
+// function get(){
+//     const listLen = document.querySelectorAll(".list li").length;
+//     for(var i = 0;i<listLen;i++){
+//         console.log(i);
+//     }
+// }
+
+// const listLen = document.querySelectorAll(".list li").length;
+// for (let i = 0; i < listLen; i++) {
+//     document.querySelectorAll(".list li")[i].addEventListener("click", function () {
+//         console.log(i + 1);
+//     })
+// }
+
+// for(var i = 0;i<10;i++){
+//     console.log(i);
+// }
+
+// var a = 10;
+// if(a > 20){
+//     var b = 100;
+// }else{
+//     var b = 200;
+// }
+// console.log(b);
+
