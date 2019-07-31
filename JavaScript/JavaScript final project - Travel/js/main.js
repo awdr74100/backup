64065539c89f9c26ee741b9b6c65e4c857f14409
藍奕濡 <a78945612385238@gmail.com>
2019-08-01 01:19:39 +0800

    20190801

--- JavaScript/JavaScript final project - Travel/js/main.js
+++ JavaScript/JavaScript final project - Travel/js/main.js
@@ -4,6 +4,12 @@ const href = "https://data.kcg.gov.tw/api/action/datastore_search?resource_id=92
 xhr.open("get", href, true);
 xhr.send(null);
 xhr.onload = function (e) {
+    // 頁面是否正常獲取
+    if (e.target.status == 404) {
+        alert("資料獲取錯誤，頁面初始化失敗");
+        return;
+    }
+
     // 宣告DOM Element
     const select = document.querySelector(".js-select");
     const list = document.querySelector(".js-list");
@@ -13,7 +19,6 @@ xhr.onload = function (e) {
     const next = document.querySelector(".next");
     const toggle = document.querySelector(".toggleList");
     const toggleBar = document.querySelector(".toggleBar");
-    const goTop = document.querySelector(".goTop");
 
     // 宣告獲取回傳資料的物件
     const jsondata = JSON.parse(xhr.responseText);
@@ -26,37 +31,43 @@ xhr.onload = function (e) {
     prev.addEventListener("click", prevPage, false);
     next.addEventListener("click", nextPage, false);
 
-    // 初始化 - 隱藏toggleBar
-    toggleBar.style.display = "none";
-
     // 宣告空陣列
     let data = [];
 
     // 宣告目前頁數
     let pageIndex;
 
-    // 創建slect清單
-    (function createSlectList() {
+    // 初始化頁面
+    (function (e) {
+        createSlectList();
+        addToObject("不限區域")
+    })();
+
+    // 讀取區域數目並創建
+    function createSlectList() {
         let str = `<option value="parset" selected>- - 請選擇行政區 - -</option>`;
         let areaList = [];
         let area = [];
+        // 創建城市區域物件
         for (let i = 0; i < jsondataLen; i++) {
             areaList.push(jsondata.result.records[i].Zone);
         }
+        // 過濾相同區域
         areaList.forEach(function (value) {
             if (area.indexOf(value) == -1) {
                 area.push(value);
             }
         })
+        // 更新slect列表
         for (let i = 0; i < area.length; i++) {
             str += `<option value="${area[i]}">${area[i]}</option>`
         }
         select.innerHTML = str;
-    })()
+    }
 
     // 檢查選擇項目 - select
     function checkSelectValue(e) {
-        var selectValue = e.target.value;
+        const selectValue = e.target.value;
         data = [];
         if (selectValue == "parset") {
             return;
@@ -65,14 +76,14 @@ xhr.onload = function (e) {
         addToObject(selectValue);
     }
 
-    // 檢查選擇項目 - click
+    // 檢查點擊項目 - click
     function checkClickValue(e) {
-        var clickValue = e.target.nodeName;
+        const clickValue = e.target.nodeName;
         data = [];
         if (clickValue !== "LI") {
             return;
         }
-        var value = e.target.textContent;
+        const value = e.target.textContent;
         toggleBar.style.display = "flex";
         addToObject(value);
     }
@@ -80,9 +91,20 @@ xhr.onload = function (e) {
     // 新增物件到陣列
     function addToObject(value) {
         title.textContent = value;
-        for (var i = 0; i < jsondataLen; i++) {
+        for (let i = 0; i < jsondataLen; i++) {
             if (value == jsondata.result.records[i].Zone) {
-                var object = {
+                let object = {
+                    img: jsondata.result.records[i].Picture1,
+                    name: jsondata.result.records[i].Name,
+                    location: jsondata.result.records[i].Zone,
+                    clock: jsondata.result.records[i].Opentime,
+                    address: jsondata.result.records[i].Add,
+                    tel: jsondata.result.records[i].Tel,
+                    price: jsondata.result.records[i].Ticketinfo,
+                }
+                data.push(object);
+            } else if (value == "不限區域") {
+                let object = {
                     img: jsondata.result.records[i].Picture1,
                     name: jsondata.result.records[i].Name,
                     location: jsondata.result.records[i].Zone,
@@ -99,17 +121,17 @@ xhr.onload = function (e) {
 
     // 創建切換列表
     function createToggleList() {
-        var dataLen = data.length;
-        var count = (dataLen / 8) + 1;
-        var str = "";
-        for (var i = 1; i <= count; i++) {
+        const dataLen = data.length;
+        const count = (dataLen / 8) + 1;
+        let str = "";
+        for (let i = 1; i <= count; i++) {
             str += '<li data-index="' + i + '">' + i + '</li>'
         }
         toggle.innerHTML = str;
         startPage();
     }
 
-    // 初始化列表
+    // 初始化選擇區域頁面
     function startPage(e) {
         pageIndex = 1;
         rmAllStyle();
@@ -144,9 +166,9 @@ xhr.onload = function (e) {
 
     // 重製索引值效果
     function rmAllStyle(e) {
-        var allIndex = document.querySelectorAll(".toggleList li");
-        var allIndexLen = allIndex.length;
-        for (var i = 0; i < allIndexLen; i++) {
+        const allIndex = document.querySelectorAll(".toggleList li");
+        const allIndexLen = allIndex.length;
+        for (let i = 0; i < allIndexLen; i++) {
             allIndex[i].classList.remove("active");
         }
         getPageIndex();
@@ -154,32 +176,33 @@ xhr.onload = function (e) {
 
     // 判斷目前頁數
     function getPageIndex() {
-        var startValue = (pageIndex - 1) * 8;;
-        var endValue = pageIndex * 8;
-        var nowList = data.slice(startValue, endValue);
-        var a = document.querySelectorAll(".toggleList li");
-        var b = a[pageIndex - 1];
-        b.classList = "active";
+        const startValue = (pageIndex - 1) * 8;;
+        const endValue = pageIndex * 8;
+        const nowList = data.slice(startValue, endValue);
+        const allIndex = document.querySelectorAll(".toggleList li");
+        const thisIndex = allIndex[pageIndex - 1];
+        thisIndex.classList = "active";
         updateList(nowList);
     }
 
     // 更新列表
     function updateList(nowList) {
-        var str = "";
-        var nowListLen = nowList.length;
-        for (var i = 0; i < nowListLen; i++) {
-            str += '<li class="list__item">' +
-                '<div class="img" style="background-image: url(' + "'" + nowList[i].img + "'" + ')">' +
-                '<h3 class="name">' + nowList[i].name + '</h3>' +
-                '<p class="location">' + nowList[i].location + '</p>' +
-                '</div>' +
-                '<div class="content">' +
-                '<p class="clock">' + nowList[i].clock + '</p>' +
-                '<p class="address">' + nowList[i].address + '</p>' +
-                '<p class="tel">' + nowList[i].tel + '</p>' +
-                '<p class="price">' + nowList[i].price + '</p>';
+        let str = "";
+        const nowListLen = nowList.length;
+        for (let i = 0; i < nowListLen; i++) {
+            str += `
+            <li class="list__item">
+                <div class="img" style="background-image: url('${nowList[i].img}')">
+                    <h3 class="name">${nowList[i].name}</h3>
+                    <p class="location">${nowList[i].location}</p>
+                </div>
+            <div class="content">
+                <p class="clock">${nowList[i].clock}</p>
+                <p class="address">${nowList[i].address}</p>
+                <p class="tel">${nowList[i].tel}</p>
+                <p class="price">${nowList[i].price}</p>
+                `
         }
         list.innerHTML = str;
     }
-
 }
