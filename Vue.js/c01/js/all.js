74a8288015a89926cd08f853d48fc8de74d178e1
藍奕濡 <a78945612385238@gmail.com>
2019-08-04 01:55:30 +0800

    20190804

--- Vue.js/c01/js/all.js
+++ Vue.js/c01/js/all.js
@@ -3,15 +3,16 @@ var app = new Vue({
     data: {
         newTodoContent: "",
         todos: [{
-            id: 85,
-            content: 48,
+            id: 007,
+            content: "--- 此為使用者測試項目，可進行刪除動作 ---",
             completed: false,
         }],
         visibility: "all",
-        cacheTodo: "",
+        cacheTodo: {},
         cacheContent: "",
     },
     methods: {
+        // 新增代辦事項
         addTodo: function () {
             let value = this.newTodoContent.trim();
             let thisId = Date.now();
@@ -25,33 +26,43 @@ var app = new Vue({
             })
             this.newTodoContent = "";
         },
+        // 移除指定代辦事項
         removeTodo: function (thisItem) {
-            let newIndex = "";
-            this.todos.forEach(function (item, index) {
-                if (item.id == thisItem.id) {
-                    newIndex = index;
-                }
-            })
+            // 使用findIndex方法
+            let newIndex = this.todos.findIndex(function (item, index) {
+                return item.id == thisItem.id;
+            });
+            // 使用forEach巡視方法
+            // this.todos.forEach(function (item, index) {
+            //     if (item.id == thisItem.id) {
+            //         newIndex = index;
+            //     }
+            // });
             this.todos.splice(newIndex, 1);
 
         },
+        // 編輯指定代辦事項
         editTodo: function (item) {
             this.cacheTodo = item;
             this.cacheContent = item.content;
         },
+        // 取消編輯
         cancelEdit: function () {
             this.cacheTodo = {};
         },
+        // 儲存已更改代辦事項
         saveTodo: function (item) {
             item.content = this.cacheContent;
             this.cacheTodo = {};
             this.cacheContent = "";
         },
+        // 清除所有代辦事項
         clearTodo: function () {
             this.todos = [];
         }
     },
     computed: {
+        // 切換代辦事項類別
         filiterTodo: function () {
             if (this.visibility == "all") {
                 return this.todos;
@@ -73,6 +84,7 @@ var app = new Vue({
                 return completedTodo;
             }
         },
+        // 計算所有未完成代辦事項
         doingTodoTotal: function () {
             let allDoingTodo = [];
             this.todos.forEach(function (item) {
