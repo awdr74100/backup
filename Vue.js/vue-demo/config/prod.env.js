294145167c57ac46bbb3bec223146174e2c524c6
藍奕濡 <a78945612385238@gmail.com>
2019-08-19 23:09:10 +0800

    20190819 vue-cli learn

---
+++ Vue.js/vue-demo/config/prod.env.js
@@ -0,0 +1,5 @@
+'use strict'
+module.exports = {
+  NODE_ENV: '"production"',
+  HTTP_PATH: '"ffFsf"',
+}
---
+++ Vue.js/vue-demo/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <HelloWorld/>
+    <h1>這是app的h1</h1>
+  </div>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  created(){
+    this.$http.get("https://randomuser.me/api/").then((response) => {
+  console.log(response.data)
+})
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
