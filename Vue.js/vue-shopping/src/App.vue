bad28e1851df66cb8f7f1f6ece635c29557ff649
藍奕濡 <a78945612385238@gmail.com>
2019-08-23 22:53:39 +0800

    20190823

---
+++ Vue.js/vue-shopping/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    // created() {
+    //   // 伺服器主機位置 + 註冊的API
+    //   const server = process.env.API_PATH;
+    //   const user = process.env.CUSTOM_PATH;
+    //   // const api = 'https://vue-course-api.hexschool.io/api/roya/products'
+    //   const api = `${server}/api/${user}/products`;
+    //   this.$http.get(api).then((response) => {
+    //     console.log(response.data)
+    //   })
+    // },
+    
+  }
+
+</script>
+
+<style lang="scss">
+  @import '@/assets/all.scss';
+
+</style>
---
+++ Vue.js/vue-shopping/src/components/HelloWorld.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="hello">
+    <a href="#" @click.prevent="signout">登出</a>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    data() {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    },
+    methods: {
+      signout() {
+        const server = process.env.API_PATH;
+        const api = `${server}/logout`;
+        const vm = this;
+
+        this.$http.post(api).then((response) => {
+          console.log(response.data)
+          if (response.data.success) {
+            vm.$router.push("/login");
+          }
+        })
+      }
+    }
+  }
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1,
+  h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+
+</style>
