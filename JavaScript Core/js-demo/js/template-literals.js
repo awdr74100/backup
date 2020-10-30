d04a7db218ccc588d6ffc63f8ebec2eaf5d57e43
藍奕濡 <a78945612385238@gmail.com>
2020-10-31 02:50:34 +0800

    add JavaScript Core Learn

--- JavaScript Core/js-demo/js/template-literals.js
+++ JavaScript Core/js-demo/js/template-literals.js
@@ -12,9 +12,34 @@ const obj = [
     age: 18,
   },
 ];
-const str = `<ul>${obj.reduce(
-  (arr, cur) => arr + `<li>${cur.name}</li>`,
-  ""
-)}</ul>`;
+// const str = `<ul>${obj.reduce(
+//   (arr, cur) => arr + `<li>${cur.name}</li>`,
+//   ""
+// )}</ul>`;
+
+// const str = `<ul>
+// ${obj
+//   .map((item) => `<li>你好，我叫 ${item.name}，年齡為 ${item.age}</li>`)
+//   .join("")}
+// </ul>`;
+// const dom = document.querySelector("#app");
+// dom.innerHTML = `${str}`;
+
+function convertString(strings, ...argv) {
+  console.log(strings);
+  console.log(argv);
+  return strings
+    .map(
+      (item, index) =>
+        `${item}${argv[index] ? `<span>${argv[index]}</span>` : ""}`
+    )
+    .join("");
+}
+
+const str = `<ul>${obj.reduce((arr, cur) => {
+  return (
+    arr + convertString`<li>你好我的名子是 ${cur.name}，年齡為 ${cur.age}</li>`
+  );
+}, "")}</ul>`;
 const dom = document.querySelector("#app");
 dom.innerHTML = `${str}`;
